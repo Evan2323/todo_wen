cf0cf3f85c8bd88c783b9760a5fa34de1b27ca7a
Ivan Petrov <>
2021-05-11 15:32:25 +0000

    добавим файлы app.py, config.py, handlers.py, дада

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "#https://api.telegram.org/bot1754648151:AAFVeVnvAlHJQ5bmacWZ4eVcWG7PLzwULQA/getUpdates"
+admin_id = 
+todo = {}
+HELP = """
+add - добавить задачу
+show - 
+done - 
+help -
